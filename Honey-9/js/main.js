1a2a0c7e06a8e4eeb54d3061ede2a7a3a87e94cc
Honey-9 <harshsingh9667903022@gmail.com>
2021-10-28 12:35:02 +0530

    Created a landing page

---
+++ Honey-9/js/main.js
@@ -0,0 +1,42 @@
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
+const aboutContent = document.querySelector("#about-content");
+const contactContent = document.querySelector("#contact-content");
+
+about.addEventListener("click", () => {
+  const aboutBox = new WinBox({
+    title: "About Me",
+    width: "400px",
+    height: "400px",
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground("#00aa00");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
+
+contact.addEventListener("click", () => {
+  const contactBox = new WinBox({
+    title: "Contact Me",
+    width: "400px",
+    height: "400px",
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground("#00aa00");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
